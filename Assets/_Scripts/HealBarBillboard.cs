2b440f7ebba174aed4e8d0831343c6772d60e74f
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-02-18 12:54:20 -0500

    added healthbar

---
+++ Assets/_Scripts/HealBarBillboard.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealBarBillboard : MonoBehaviour
+{
+    public GameObject playerCamera;
+    public Transform camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        playerCamera = GameObject.Find("PlayerCamera");
+        camera = playerCamera.GetComponent<Camera>().transform;
+    }
+
+
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + camera.forward);
+    }
+}
